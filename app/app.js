8a3aad07d6e3e84ab80c0fe51c50acdb17270493
ShestakovDmitry <priempochty@yandex.ru>
2015-09-28 11:43:09 +0500

    Приложение работающее с LocalStorage
    Приложение работающее с LocalStorage. Есть возможность добавить,
    удалить, изменить запись.

---
+++ app/app.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+ 
+/* App Module */
+
+angular.module('crudtableApp', [
+  'ngRoute',
+  'crudtableControllers'
+])
+    .config(
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'app/partials/main.html',
+                controller: 'MainController'
+            }).
+            when('/users/edit/:userId', {
+                templateUrl: 'app/partials/edit.html',
+                controller: 'EditController'
+            }).
+            when('/users/add', {
+                templateUrl: 'app/partials/add.html',
+                controller: 'AddController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }
+);
